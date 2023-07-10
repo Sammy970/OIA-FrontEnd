a10e39ff7fc0bae254567e0e514b2461e7955a72
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-10 18:27:37 +0530

    Cleaned Boilerplate Code with Auth0

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
-        <LoginButton />
-        <br />
-        <LogoutButton />
         <Profile />
+        <br />
+        {!isAuthenticated ? <LoginButton /> : <LogoutButton />}
       </header>
     </div>
   );
