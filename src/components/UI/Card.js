ab646cb1bde6fc0647fb30ca9e370527ae1e81d4
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-11 07:57:47 +0530

    Displayed short link

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -9,7 +9,7 @@ const Card = ({ data }) => {
         data.map((d) => {
           return (
             <p key={d.code}>
-              {d.code} - {d.link}
+              {d.code} - https://oia-vercel.app/{d.code} - {d.link}
             </p>
           );
         })
